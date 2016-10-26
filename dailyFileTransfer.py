266134f0a75ef3c780db2901559903156d048677
JeremyADreher <jeremyadreher@gmail.com>
2016-10-26 10:29:41 -0700

    Second commit: edited code into functions to make it modular

--- dailyFileTransfer.py
+++ dailyFileTransfer.py
@@ -1,23 +1,26 @@
 import os
 import time
-from datetime import datetime, timedelta
 import shutil
 
 
-sourcePath = 'C:\\LocalSourceFiles\\Python_Daily_File_Transfer\\Daily'
-destPath = 'C:\\LocalSourceFiles\\Python_Daily_File_Transfer\\Home_Office'
-source = os.listdir(sourcePath)
 
-
-
-for files in source:
-            source_files = os.path.join(sourcePath, files)
-            dest_files = os.path.join(destPath, files)
-            modTime = os.path.getmtime(source_files)
-            currentTime = time.time()
-            twentyFourHoursAgo = currentTime - 86400
-            if files.endswith('.txt') and modTime > twentyFourHoursAgo:
+def move_files(sourcePath, destPath):
+    source = os.listdir(sourcePath)
+    for files in source:
                 source_files = os.path.join(sourcePath, files)
                 dest_files = os.path.join(destPath, files)
-                shutil.copy(source_files,destPath)
+                modTime = os.path.getmtime(source_files)
+                currentTime = time.time()
+                twentyFourHoursAgo = currentTime - 86400
+                if files.endswith('.txt') and modTime > twentyFourHoursAgo:
+                    source_files = os.path.join(sourcePath, files)
+                    dest_files = os.path.join(destPath, files)
+                    shutil.copy(source_files,destPath)
 
+def main():
+    src = 'C:\\LocalSourceFiles\\Python_Daily_File_Transfer\\Daily'
+    dst = 'C:\\LocalSourceFiles\\Python_Daily_File_Transfer\\Home_Office'
+    move_files(src,dst)
+
+if __name__=='__main__':
+    main()
